180eb6821ba525fff22ace85e8d24827f84ef740
Akande Al-ameen <longlasting1805@gmail.com>
2022-06-01 09:15:28 +0100

    model design

--- myBlog/models.py
+++ myBlog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=1000, default='')
+    author = models.TextField()
+    comment = models.TextField(max_length=1000, unique=True)
+
+    def __str__(self):
+        return self.title
