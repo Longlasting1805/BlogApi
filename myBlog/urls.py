15efa4c79e154edebc46c420a21b0eeddc175f20
Akande Al-ameen <longlasting1805@gmail.com>
2022-06-02 20:03:54 +0100

    view

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 
-from .views import BlogView
+from .views import BlogApiView
 
 router = DefaultRouter()
-router.register('blog', BlogView)
+# router.register('blog', BlogApiView, basename='blog')
 
 urlpatterns = [
-    path('blog/', include(router.urls)),
-    path('blog/', BlogView.as_view())
+    path('/', include(router.urls)),
+    # path('detail/<int:id>/', BlogApiView.as_view()),
+    path('blog/', BlogApiView.as_view())
 ]
--- myBlog/views.py
+++ myBlog/views.py
@@ -1,5 +1,5 @@
-
 import view
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
@@ -10,7 +10,7 @@ from myBlog.models import Blog
 from myBlog.serializers import BlogSerializer
 
 
-class BlogView(APIView):
+class BlogApiView(APIView):
     def get(self, request):
         articles = Blog.objects.all()
         serializer = BlogSerializer(articles, many=True)
@@ -24,3 +24,29 @@ class BlogView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+# class blogDetailApi(APIView):
+#     def get_object(self, id):
+#         try:
+#             return Blog.objects.get(id=id)
+#
+#         except Blog.DoesNotExist:
+#             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+#
+#     def get(self, request, id):
+#         article = self.get_object(id)
+#         serializer = BlogSerializer(article)
+#         return Response(serializer.data)
+#
+#     def put(self, request, id):
+#         article = self.get_object(id)
+#         serializer = BlogSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, id):
+#         article = self.get_object(id)
+#         article.delete()
+#         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
